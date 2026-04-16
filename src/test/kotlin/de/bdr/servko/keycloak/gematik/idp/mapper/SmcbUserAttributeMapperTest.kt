d5f4ca0c99bcb0bd2a9cd9b29b5ab12997dd8e46
Josip Lausic <josip.lausic@maurer-electronics.hr>
2026-04-16 13:09:50 +0200

    Release 3.13.0

--- src/test/kotlin/de/bdr/servko/keycloak/gematik/idp/mapper/SmcbUserAttributeMapperTest.kt
+++ src/test/kotlin/de/bdr/servko/keycloak/gematik/idp/mapper/SmcbUserAttributeMapperTest.kt
@@ -1,19 +1,6 @@
 /*
- *  Copyright 2023 Bundesdruckerei GmbH and/or its affiliates
- *  and other contributors.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
+ * Copyright 2026 Bundesdruckerei GmbH
+ * For the license, see the accompanying file LICENSE.md
  */
 
 package de.bdr.servko.keycloak.gematik.idp.mapper
@@ -27,6 +14,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.keycloak.broker.provider.BrokeredIdentityContext
 import org.keycloak.models.*
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
@@ -35,7 +23,7 @@ import org.mockito.kotlin.verify
 internal class SmcbUserAttributeMapperTest {
 
     private val session: KeycloakSession = mock()
-    private val config: GematikIDPConfig = mock()
+    private val idpConfig: GematikIDPConfig = mock() { on { isEnabled } doReturn true }
 
     private val realm: RealmModel = mock()
 
@@ -55,7 +43,7 @@ internal class SmcbUserAttributeMapperTest {
                 AbstractGematikIDPUserAttributeMapper.CONFIG_CLAIM_ATTRIBUTE to GematikClaim.TELEMATIK_ID.name
             )
         }
-        context = BrokeredIdentityContext("id", config).apply {
+        context = BrokeredIdentityContext("id", idpConfig).apply {
             contextData[ContextData.CONTEXT_SMCB_TELEMATIK_ID.name] = smcbValue
         }
     }
